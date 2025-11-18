2710243137ba7d63f6ab2b73742ab46d348a6c07
MateuszW <u56172@student.wat.edu.pl>
2025-11-18 12:29:18 +0100

    chore: Added initial requirements for project dependencies

---
+++ requirements.py
@@ -0,0 +1,15 @@
+# Aktywne pakiety
+cartopy==0.25.0
+matplotlib==3.10.8
+netCDF4==1.7.3
+numpy==2.3.4
+pandas==2.3.3
+pyqt5==5.15.11
+python==3.12.12
+requests==2.32.5
+xarray==2025.10.1
+
+# Pakiety zainstalowane, ale obecnie nieużywane (potencjalne rozwinięcie aplikacji)
+# metpy==1.7.1
+# scipy==1.16.3
+# siphon==0.10.0
